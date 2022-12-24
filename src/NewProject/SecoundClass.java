16aea15965edf29182de414bff5374f5558c5b6d
Arslanullah <114203538+Arslanullah@users.noreply.github.com>
2022-12-24 15:03:22 -0600

    secound git project ..changes made

--- src/NewProject/SecoundClass.java
+++ src/NewProject/SecoundClass.java
@@ -1,9 +1,12 @@
+package NewProject;
+
 public class SecoundClass {
 
     public static void main(String[] args) {
 
 
         System.out.println("seound project for git live satuirday class");
+        System.out.println("chanmges made");
     }
 }
 
